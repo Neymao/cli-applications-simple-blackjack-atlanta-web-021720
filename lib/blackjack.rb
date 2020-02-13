236e0ba0a45340baf3047230113c4b14edfd54de
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-02-13 02:12:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,13 +53,16 @@ def invalid_command
   
 end
 
-def runner
-  welcome
-  hand=initial_round
-  until hand>21 do
-    hand=hit?(hand)
-    display_card_total(hand)
-  end
-  end_game(hand)
-end
-end
+#def runner
+ # welcome
+  #hand=initial_round
+  #until hand>21 do
+    #hand=hit?(hand)
+    #display_card_total(hand)
+  #end
+  #end_game(hand)
+#end
+#end
+
+
+
